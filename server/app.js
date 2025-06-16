1c9e1d7114b586c9f7ddc6a1c2f2de6cae34f531
addiayoub <addiayoub2005@gmail.com>
2025-06-16 02:42:29 +0100

    commit .js

--- server/app.js
+++ server/app.js
@@ -58,7 +58,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; img-src 'self' https://api.nhancit.com data: blob:;"
+  );
+  next();
+});
 // Middleware pour gérer les requêtes OPTIONS manuellement (fallback)
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
